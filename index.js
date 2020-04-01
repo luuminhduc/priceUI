66eb7b0a56d96e49d917e01e70f97c872fc551b5
Duc <luuminhduc00@gmail.com>
2020-04-01 10:53:23 +0700

    Changed index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('input');
+const ball = document.querySelector('.toggle-label');
+const toggleContainer = document.querySelector('.toggle-container');
+const prices = document.querySelectorAll('.price .price-number');
+
+toggleContainer.addEventListener('click', (e) => {
+    console.log(input.checked);
+    
+})
