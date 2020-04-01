79279f6e741538e47b3132c76ecbb2c3145a825b
Duc <luuminhduc00@gmail.com>
2020-04-01 11:23:15 +0700

    Changed index.html

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const toggleContainer = document.querySelector('.toggle-container');
 const prices = document.querySelectorAll('.price .price-number');
 
 toggleContainer.addEventListener('click', (e) => {
-    console.log(input.checked);
-    
-})
+    toggleContainer.classList.toggle('active');
+
+    prices.forEach(el => {
+        const currentPrice = el.innerText.replace('$', '');
+        const annualPrice = el.getAttribute('target');
+        const monthlyPrice = el.getAttribute('target') / 12;
+        if(toggleContainer.classList.contains('active')) {
+            el.innerText = monthlyPrice.toFixed(2) + '$';
+        }else{
+            el.innerText = annualPrice + '$';
+            console.log(annualPrice);
+        }
+        
+    })
+});
+console.log('a');
